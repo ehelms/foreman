c7cd8a747a643929e6981fc27c9f1f5a6cfbe574
Eric D. Helms <ericdhelms@gmail.com>
2014-06-24 19:40:23 +0100

    Refs #5209: Adds ability to specify admin user properties to db:seed.

--- db/seeds.d/12-auth_sources.rb
+++ db/seeds.d/12-auth_sources.rb
@@ -3,13 +3,19 @@ AuthSource.without_auditing do
   src = AuthSourceInternal.find_by_type "AuthSourceInternal"
   src ||= AuthSourceInternal.create :name => "Internal"
 
+  admin_firstname = ENV['admin_username'] || 'Admin'
+  admin_lastname = ENV['admin_lastname'] || 'User'
+  admin_login = ENV['admin_login'] || 'admin'
+  admin_password = ENV['admin_password'] || 'changeme'
+  admin_email = ENV['admin_email'] || Setting[:administrator]
+
   # Users
-  unless User.find_by_login("admin").present?
+  unless User.find_by_login(admin_login).present?
     User.without_auditing do
-      user = User.new(:login => "admin", :firstname => "Admin", :lastname => "User", :mail => Setting[:administrator])
+      user = User.new(:login => admin_login, :firstname => admin_firstname, :lastname => admin_lastname, :mail => admin_email)
       user.admin = true
       user.auth_source = src
-      user.password = "changeme"
+      user.password = admin_password
       User.current = user
       raise "Unable to create admin user: #{format_errors user}" unless user.save
     end
